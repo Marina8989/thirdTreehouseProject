4f843b7052d9df19cca54763dcdb8f9a1e1307f4
Marina8989 <marina.mill209@gmail.com>
2021-05-09 18:44:16 -0700

    added html css and js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const btn = document.querySelector('.btn');
 const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    // Name input check
+    // Name input valildation
     if(nameInput.value == "") {
      nameInput.classList.add('redInput');
      nameInput.setAttribute('placeholder',`Oops, this field is still empty`);
   }
+
+    // Email input validation 
+    if(emailInput.value == "" || !(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailInput.value))) {
+       emailInput.classList.add('redInput');
+       emailInput.value = '';
+       emailInput.setAttribute('placeholder', `You have entered an invalid email address!`);
+    }  
 })
 
