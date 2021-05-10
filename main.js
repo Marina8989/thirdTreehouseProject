c529f9fed231c03852e5c9939db961275b99d326
Marina8989 <marina.mill209@gmail.com>
2021-05-09 22:58:15 -0700

    js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ btn.addEventListener('click', (e) => {
 // title input
 const optionTitle = document.getElementById('option');
 const optionInput = document.querySelector('.optionInput');
-console.log(optionTitle);
+
 optionTitle.addEventListener('change', () => {
     if(optionTitle.value == 'other'){
         optionInput.style.display = 'block';
@@ -55,6 +55,25 @@ design.addEventListener('change', () => {
          colorTwo.style.display = 'block';
          colorOne.style.display = 'none';
     }
+})
+
+// total amout for activities
+const checkboxInput = document.querySelectorAll('.checkboxes input');
+const checkboxLabel = document.querySelectorAll('label');
+const checkBox = document.querySelector('.checkboxes');
+const spanText = document.querySelector('.totalAmount');
+const totalText = document.querySelector('.total');
+
+
+checkboxInput.forEach(item => {
+    item.addEventListener('change', (e) => {
+        if(e.target.id == 'main'){
+           console.log(e.target)
+           spanText.innerText = '$'+ Number(200);
+           totalText.append(spanText);
+           totalText.style.display = 'block';
+        }
+    })
 })
 
 
