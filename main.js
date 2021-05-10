fd229261fd199cd076bf32e42d08ec34a106a5df
Marina8989 <marina.mill209@gmail.com>
2021-05-10 14:15:41 -0700

    third project from tree house

--- main.js
+++ main.js
@@ -21,14 +21,11 @@ btn.addEventListener('click', (e) => {
        emailInput.value = '';
        emailInput.setAttribute('placeholder', `You have entered an invalid email address!`);
     }  
-
-
-
-
     // empty input after submit
      nameInput.value = '';
      emailInput.value = '';
 })
+
 
 // title input
 const optionTitle = document.getElementById('option');
@@ -84,8 +81,6 @@ checkboxInput.forEach(item => {
 
 
 // payment option
-
-
 //paypal option
 const payment = document.getElementById('payment');
 const paypal = document.querySelector('.paypal');
@@ -109,11 +104,31 @@ payment.addEventListener('change', () => {
        paypal.style.display = 'none';
        creditCard.style.display = 'flex';
        date.style.dysplay = 'flex';
+
+       valid_credit_card(document.querySelector('.creditCardInput'))
     }
 })
 
 
-//credit card
+function valid_credit_card(value) {
+	  // accept only digits, dashes or spaces
+		if (/[^0-9-\s]+/.test(value)) return false;
+		if (value === "") return false; 
+		// The Luhn Algorithm. It's so pretty.
+		var nCheck = 0, nDigit = 0, bEven = false;
+		value = value.replace(/\D/g, "");
+		for (var n = value.length - 1; n >= 0; n--) {
+			var cDigit = value.charAt(n);
+				nDigit = parseInt(cDigit, 10);
+
+			if (bEven) {
+				if ((nDigit *= 2) > 9) nDigit -= 9;
+			}
+			nCheck += nDigit;
+			bEven = !bEven;
+		}
+		return (nCheck % 10) === 0;
+	}
 
 
  
